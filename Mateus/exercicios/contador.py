ba3f2315e114a1b560bc3ad3d5de270c132b60d8
Mateus Souza Oliveira <msouzaol@emeal.nttdata.com>
2022-07-22 15:37:37 -0300

    aula1 git

---
+++ Mateus/exercicios/contador.py
@@ -0,0 +1,4 @@
+num_ = int(input('Digite um num qualquer: '))
+for i in range(-1, num_):
+    i += 1
+    print(i)
---
+++ Mateus/exercicios/desafio2.py
@@ -0,0 +1,6 @@
+desafio2 = open('arquivo.txt', 'r')
+minha_lista = []
+minha_lista.append(desafio2.read())
+
+meudicionario = {'mateus': minha_lista}
+print(minha_lista)
